aeed26a7af57cc266dda4bdff9c47272d105f725
gbrockst <55106931+gbrockst@users.noreply.github.com>
2019-09-17 15:01:56 -0400

    Delete Week 2 R Exercise.r

--- Week 2 R Exercise.r
+++
@@ -1,9 +0,0 @@
-
-#take our R script that works and upload it into github so jeff can view it
-
-#Question 1
-2^5
-
-peanut.weight <- c(89.4, 78.2, 91.4, 95.2, 105.5)
-
-names <- c(Sullivan, Bailey, Bailey II, Emery, Wynne)
